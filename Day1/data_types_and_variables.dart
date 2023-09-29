99f2d1ce4c4af0793d43d7481034cd0aeb30a33d
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-09-29 09:33:54 +0545

    datatypes and variables in dart

---
+++ Day1/data_types_and_variables.dart
@@ -0,0 +1,20 @@
+void main(){
+  // data type - those pre building data types that represents data what kind of data it is
+  // Generally, in dart there are 7 building data types. They are as follows:
+  int age = 18;
+  // In this example the value 18 is integer and age contains the value 18 and is called variable and that variable should be integer type to store that integer value
+  double salary = 8848.48;
+  // In tis example, the double data type is shown with its floating value.
+  String name = "Nabin Timsina";
+   
+   num height = 5.32 ;
+
+   bool isRaining = true;
+
+   print(age);
+   print(salary);
+   print("name");
+   print(height);
+   print(isRaining);
+
+}
