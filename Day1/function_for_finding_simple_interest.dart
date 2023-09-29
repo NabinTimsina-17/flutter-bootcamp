5814b3ec28ae1438ee27f5effa3108d0fafe1a11
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-09-29 18:59:56 +0545

    updated

--- Day1/function_for_finding_simple_interest.dart
+++ Day1/function_for_finding_simple_interest.dart
@@ -2,7 +2,6 @@ void calculateInterest(double principal,double rate, double time){
   double interest = principal * rate * time / 100;
   print("Simple Interest is $interest");
 }
-
 void main(){
   double principal = 10000;
   double time = 3;
