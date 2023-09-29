9b7756df7930eb0c037891bdbfefdc9d71df05db
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-09-29 10:00:19 +0545

    operators in dart

---
+++ Day1/operators.dart
@@ -0,0 +1,18 @@
+void main(){
+  int num1 = 5;
+  int num2 = 10;
+
+int sum1 = num1 + num2;
+int sum2 = num1 - num2;
+int sum3 = num1 * num2;
+double sum4 = num1 / num2;
+int sum5 = num1 % num2;
+int sum6 = num1 ~/ num2;
+
+print(sum1);
+print(sum2);
+print(sum3);
+print(sum4);
+print(sum5);
+print(sum6);
+}
