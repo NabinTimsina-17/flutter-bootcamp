6e1edb7623e4ae4a38611882d64f16c240e8d5be
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-09-29 12:19:52 +0545

    split string in dart

---
+++ Day1/split_string.dart
@@ -0,0 +1,10 @@
+void main() {
+  String allNames = "Ram, Krishna, Buddha, Kalki";
+
+  List<String> listNames = allNames.split(",");
+  print("List Name at 0 index ${listNames[0]} ");
+  print("List Name at 1 index ${listNames[1]} ");
+  print("List Name at 2 index ${listNames[2]} ");
+  print("List Name at 3 index ${listNames[3]} ");
+
+}
