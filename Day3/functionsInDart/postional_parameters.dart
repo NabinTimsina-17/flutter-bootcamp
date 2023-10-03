a06823cf1189d54001f1ab8a2a6f722078c9f3dc
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-10-03 15:26:37 +0545

    positional parameters in dart

---
+++ Day3/functionsInDart/postional_parameters.dart
@@ -0,0 +1,8 @@
+void fullName(String firstName, String LastName){
+  print("Your full name is $firstName $LastName");
+}
+
+void main(){
+    fullName("Nabin", "Timsina");
+    fullName("John","Abraham");
+  }
