051f53f2dd64c92980f7895f05e6ab9ecb2b7977
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-10-04 14:22:27 +0545

    check if the list is empty or not

---
+++ Day4/dartCollection/LIST/check_if_list_is_empty_or_not.dart
@@ -0,0 +1,8 @@
+void main(){
+  List<String> drinks = ["water","juice","milk","coke"];
+  List<int> ages = [1];
+  print("Is drinks empty: "+drinks.isEmpty.toString());
+  print("Is drinks not empty: "+drinks.isNotEmpty.toString());
+  print("Is ages Empty: "+ages.isEmpty.toString());
+  print("Is ages not empty: "+ages.isNotEmpty.toString());
+}
