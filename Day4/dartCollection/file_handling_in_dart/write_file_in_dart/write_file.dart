1e86f90b0cb2167e2ff974c50d38c11f71469302
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-10-07 23:41:55 +0545

    write file in dart

---
+++ Day4/dartCollection/file_handling_in_dart/write_file_in_dart/write_file.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() async {
+  // open file
+  File file = File('test.txt');
+
+  try {
+    // write to file asynchronously
+    await file.writeAsString('Welcome to test.txt file.');
+    print('Heelllloooooo subekshya how are you? Hope you are fine. This is your loving and caring boyfriend. ');
+  } catch (e) {
+    print('Error writing to file: $e');
+  }
+}
