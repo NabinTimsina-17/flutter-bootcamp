31f6929021fee78a6ceaaf18a3a00979979a7ffe
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-10-11 08:01:01 +0545

    updated

--- Day4/dartCollection/file_handling_in_dart/write_file_in_dart/write_file.dart
+++ Day4/dartCollection/file_handling_in_dart/write_file_in_dart/write_file.dart
@@ -6,7 +6,7 @@ void main() async {
   try {
     // write to file asynchronously
     await file.writeAsString('Welcome to test.txt file.');
-    print('Heelllloooooo subekshya how are you? Hope you are fine. This is your loving and caring boyfriend. ');
+    print('Heelllloooooo how are you? Hope you are fine ');
   } catch (e) {
     print('Error writing to file: $e');
   }
