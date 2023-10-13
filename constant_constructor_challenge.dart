c87ddb5b9b2b418fe7852c3665c1151897191ee5
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-10-13 08:39:09 +0545

    challenge for constant constructor in dart

---
+++ constant_constructor_challenge.dart
@@ -0,0 +1,17 @@
+import 'Day6/oops/constructor3.dart';
+
+class Customer{
+  final String? name;
+  final int? age;
+  final int? phone;
+
+  // Constant   Constructor
+  const Customer({this.name, this.age, this.phone});
+}
+
+void main(){
+  const  Customer customer = Customer(name :"John Doe" , age : 26, phone: 9841023561);
+  print("Name is: ${customer.name}");
+  print("Age: ${customer.age}");
+  print("Prize: ${customer.phone}");
+}
